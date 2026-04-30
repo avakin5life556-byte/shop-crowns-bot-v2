7a0d0b23c313b8727e20337c2fd6bb8b07cf98cc
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 13:40:32 +0300

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -1,7 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from bot.config import BOT_TOKEN
+from aiogram.fsm.storage.memory import MemoryStorage
 
 bot = Bot(token=BOT_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(storage=MemoryStorage())
