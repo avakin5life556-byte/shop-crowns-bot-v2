62b34a4c1ef4f781dbf5359ac1fe44c47960f003
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 17:22:22 +0300

    Update models.py

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
+from bot.dataclasses import dataclass
+from bot.datetime import datetime
+from bot.typing import Optional
 
 @dataclass
 class User:
