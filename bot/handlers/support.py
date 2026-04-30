e4bcfbe1508a9a81d097a833181c08b87e6f9c77
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 17:41:27 +0300

    Update support.py

--- bot/handlers/support.py
+++ bot/handlers/support.py
@@ -1,12 +1,12 @@
-from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery
-from aiogram.fsm.context import FSMContext
-from database import db
-from keyboards.inline import get_live_chat_keyboard, get_support_rating_keyboard
-from keyboards.reply import get_main_keyboard
-from states.support_states import ComplaintStates
-from config import ADMIN_ID
-from datetime import datetime
+from bot.aiogram import Router, F
+from bot.aiogram.types import Message, CallbackQuery
+from bot.aiogram.fsm.context import FSMContext
+from bot.database import db
+from bot.keyboards.inline import get_live_chat_keyboard, get_support_rating_keyboard
+from bot.keyboards.reply import get_main_keyboard
+from bot.states.support_states import ComplaintStates
+from bot.config import ADMIN_ID
+from bot.datetime import datetime
 
 router = Router()
 
