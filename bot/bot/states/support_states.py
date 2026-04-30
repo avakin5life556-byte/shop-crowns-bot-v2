2775b15d34e2a2efeaa5e1ae66464b38d6513134
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 16:45:54 +0300

    Delete bot/bot/states directory

--- bot/bot/states/support_states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class ComplaintStates(StatesGroup):
-    WAITING_MESSAGE = State()
-
-
-class SupportStates(StatesGroup):
-    LIVE_CHAT = State()
-    WAITING_REPLY = State()
