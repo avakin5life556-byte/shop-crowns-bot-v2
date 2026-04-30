3c8181a5f1a949246c32255b0206ee27d66ea937
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 16:32:19 +0300

    Create complaint_states.py

---
+++ bot/states/complaint_states.py
@@ -0,0 +1,8 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class ComplaintStates(StatesGroup):
+    """حالات قسم الشكاوى"""
+    WAITING_MESSAGE = State()      # انتظار رسالة الشكوى
+    LIVE_CHAT = State()            # شات مباشر نشط
+    ADMIN_REPLYING = State()       # الأدمن يرد
