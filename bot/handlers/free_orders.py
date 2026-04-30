03186d8d4272d26c00cce7122b797f8b13d2e52a
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-30 19:06:27 +0300

    Update free_orders.py

--- bot/handlers/free_orders.py
+++ bot/handlers/free_orders.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-from bot.datetime import datetime
+from datetime import datetime
 import json
 import pytz
 from bot.database import db
