c1329931c9607712df9376917519a286d922629f
avakin5life556-byte <avakin5life556@gmail.com>
2026-05-02 13:58:43 +0300

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -1,7 +1,9 @@
 import logging
+
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.config import BOT_TOKEN
 
@@ -10,8 +12,9 @@ logger = logging.getLogger(__name__)
 # تهيئة البوت
 bot = Bot(
     token=BOT_TOKEN,
-    parse_mode=ParseMode.MARKDOWN,
-    protect_content=False
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.MARKDOWN
+    )
 )
 
 # FSM Storage
